31395b5d20e30edd99e3cbb35940015b454da262
Nurzhan Dzhumanov <nurzhan_dzh@list.ru>
2022-05-07 14:51:23 +0300

    add method BruteForce

--- src/cryptoanalizer/CaesarCipher.java
+++ src/cryptoanalizer/CaesarCipher.java
@@ -5,13 +5,13 @@ import java.io.*;
 public class CaesarCipher {
     static final String RU_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
 
-    static char cipher(char symbol, int key) {
+    private static char cipher(char symbol, int key) {
         if (RU_ALPHABET.indexOf(symbol) != -1) {
             return RU_ALPHABET.charAt((RU_ALPHABET.indexOf(symbol) + key) % RU_ALPHABET.length());
         } else return symbol;
     }
 
-    static char decipher(char symbol, int key) {
+    private static char decipher(char symbol, int key) {
         if (RU_ALPHABET.indexOf(symbol) != -1) {
             int index = (RU_ALPHABET.indexOf(symbol) - key) % RU_ALPHABET.length();
             if (index < 0) {
@@ -66,5 +66,27 @@ public class CaesarCipher {
             System.out.println("Error: " + e);
         }
 
+    }
+
+    public void methodBruteForce(String Path) {
+        try (FileReader fileReader = new FileReader(Path);
+             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+             FileWriter fileWriter = new FileWriter("src/files/bruteforce-file.txt");
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+
+            String line = bufferedReader.readLine().toLowerCase();
+            for (int i = 0; i < RU_ALPHABET.length(); i++) {
+                bufferedWriter.write("Ключ "+ i + ": ");
+                for (int j = 0; j < line.length(); j++) {
+                    bufferedWriter.write(decipher(line.charAt(j), i));
+                }
+                bufferedWriter.newLine();
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
--- src/cryptoanalizer/Start.java
+++ src/cryptoanalizer/Start.java
@@ -3,17 +3,25 @@ package cryptoanalizer;
 import java.util.Scanner;
 
 public class Start {
+
+    public static Scanner scan = new Scanner(System.in);
+    public static CaesarCipher caesar = new CaesarCipher();
+
     public static void main(String[] args) {
 
-        CaesarCipher caesar = new CaesarCipher();
-        Scanner scan = new Scanner(System.in);
-        System.out.println("start");
+        printMenu();
+        printChooseMenu();
 
+    }
 
+    private static void printMenu () {
         System.out.println("Добро пожаловать в Криптоанализатор! \nЧто вы хотите сделать? ");
         System.out.println("1: Зашифровать данные!\n2: Расшифровать данные!");
         System.out.println("3: Расшифровать данные методом \"brute force\"!");
         System.out.print("4: Выход!\nВыберите действия: ");
+    }
+
+    private static void printChooseMenu() {
         int choose = scan.nextInt();
         switch (choose) {
             case 1:
@@ -26,13 +34,17 @@ public class Start {
                 break;
             case 2:
                 System.out.print("Введите путь к файлу: ");
-                String path1 = scan.next();
+                String path2 = scan.next();
                 System.out.print("Введите ключ: ");
-                int newKey = scan.nextInt();
-                caesar.decoding(path1, newKey);
-                System.out.println("Ваш зашифрованный файл находится в: src/files/decrypted-file.txt");
+                int key2 = scan.nextInt();
+                caesar.decoding(path2, key2);
+                System.out.println("Ваш расшифрованный файл находится в: src/files/decrypted-file.txt");
                 break;
             case 3:
+                System.out.print("Введите путь к файлу: ");
+                String path3 = scan.next();
+                caesar.methodBruteForce(path3);
+                System.out.println("Ваш расшифрованный файл находится в: src/files/bruteforce-file.txt");
                 break;
             case 4:
                 System.out.println("До свиданья!");
@@ -40,6 +52,5 @@ public class Start {
             default:
                 System.out.println("Вы ввели некорретную цифру.");
         }
-
     }
 }
